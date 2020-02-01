5d11a217168e629710f9ae2ecffc96cfa4f7faa2
GCreeper00 <33402504+GCreeper00@users.noreply.github.com>
2020-02-01 15:04:25 -0500

    Cities now have names

--- army.js
+++ army.js
@@ -12,31 +12,31 @@ document.write("<button onClick='infoBox = true;' id='open-info-box-button'> <p 
 
 /* Pause/Play button */
 document.write("<div id='time-controls' style='position: fixed; right: 10px; bottom: 20px; display: flex; width: 50px; height: 25px; justify-content: space-evenly;'>" +
-							 "<button onClick=\"pause = !pause; var el = document.getElementById('pause-play-button'); if (el.innerText == '||') {el.innerText = '\u1405'; el.setAttribute('style', 'position: absolute; top: 0px; font-weight: lighter; margin-left: 0px; margin-top: 4px;');} else {el.innerText='||'; el.setAttribute('style', 'position: absolute; top: 0px; font-weight: bold; margin-left: 1px; margin-top: 5px;');}\" style='outline: none; border-radius: 5px; position: block; width: 25px; height: 25px;'> <p id='pause-play-button' style='position: absolute; top: 0px; font-weight: bold; margin-left: 1px; margin-top: 5px;'>||</p> </button>" +
-							 /*"<button style='position: block; width: 25px; height: 25px;'> <p id='pause-play-button' style='position: absolute; top: 0px; font-weight: lighter; margin-left: -6px;'>\u1405\u1405</p> </button>" +
-						*/ "" +
-							 "" +
-							 "" +
-							 "</div>");
+	"<button onClick=\"pause = !pause; var el = document.getElementById('pause-play-button'); if (el.innerText == '||') {el.innerText = '\u1405'; el.setAttribute('style', 'position: absolute; top: 0px; font-weight: lighter; margin-left: 0px; margin-top: 4px;');} else {el.innerText='||'; el.setAttribute('style', 'position: absolute; top: 0px; font-weight: bold; margin-left: 1px; margin-top: 5px;');}\" style='outline: none; border-radius: 5px; position: block; width: 25px; height: 25px;'> <p id='pause-play-button' style='position: absolute; top: 0px; font-weight: bold; margin-left: 1px; margin-top: 5px;'>||</p> </button>" +
+	/*"<button style='position: block; width: 25px; height: 25px;'> <p id='pause-play-button' style='position: absolute; top: 0px; font-weight: lighter; margin-left: -6px;'>\u1405\u1405</p> </button>" +
+	*/ "" +
+	"" +
+	"" +
+	"</div>");
 
 /* Capture City Alert */
 document.write("<div id=display-options style='display: none; left: " + (window.innerWidth/2 - 250) + "px; top: " + (window.innerHeight/2 - 150) + "px; border-radius: 10px; position: fixed; height: 300px; width: 500px; background-color: black;'>" +
-							 "<div style='border-radius: 10px; position: relative; top: 1px; left: 1px; width: 498px; height: 298px; background-color: white;'>" +
-							 "<h1 style='margin-top: 0px; font-size: 60px; text-align: center; width: 100%; position: absolute;'> City Options </h1>" +
-							 "<div style='display: flex; width: 100%; position: absolute; justify-content: space-evenly; top: 80px;'>" +
-							 "<button style='outline: none; width: 200px; height: 100px; bottom: 10px; border-radius: 10px;'> <h1>Capture City</h1> </button>" +
-							 "<button onClick=\"document.getElementById('display-options').setAttribute('style', 'display: none; left: ' + (window.innerWidth/2 - 250) + 'px; top: ' + (window.innerHeight/2 - 150) + 'px; border-radius: 10px; position: fixed; height: 300px; width: 500px; background-color: black;'); pause = false; army.troops += map.cities[citySelected].people/3; army.food += map.cities[citySelected].food; map.cities.splice(citySelected, 1); citySelected = -1;\" style='outline: none; width: 200px; height: 100px; bottom: 10px; border-radius: 10px;'> <h1>Raze City</h1> </button>" +
-							 "</div>" +
-							 "<div style='display: flex; width: 100%; position: absolute; justify-content: space-evenly; top: 180px;'>" +
-							 "<div style='width: 40%;'>" +
-							 "<p>Make the city yours. It will give you food and some troops over time.</p>" +
-							 "</div>" +
-							 "<div style='width: 40%;'>" +
-							 "<p>Recruit all eligible people into your army, steal all the food, and burn the city down.</p>" +
-							 "</div>" +
-							 "</div>" +
-							 "</div>" +
-							 "</div>");
+	"<div style='border-radius: 10px; position: relative; top: 1px; left: 1px; width: 498px; height: 298px; background-color: white;'>" +
+	"<h1 style='margin-top: 0px; font-size: 60px; text-align: center; width: 100%; position: absolute;'> City Options </h1>" +
+	"<div style='display: flex; width: 100%; position: absolute; justify-content: space-evenly; top: 80px;'>" +
+	"<button style='outline: none; width: 200px; height: 100px; bottom: 10px; border-radius: 10px;'> <h1>Capture City</h1> </button>" +
+	"<button onClick=\"document.getElementById('display-options').setAttribute('style', 'display: none; left: ' + (window.innerWidth/2 - 250) + 'px; top: ' + (window.innerHeight/2 - 150) + 'px; border-radius: 10px; position: fixed; height: 300px; width: 500px; background-color: black;'); pause = false; army.troops += map.cities[citySelected].people/15; army.food += map.cities[citySelected].food; map.cities.splice(citySelected, 1); citySelected = -1;\" style='outline: none; width: 200px; height: 100px; bottom: 10px; border-radius: 10px;'> <h1>Raze City</h1> </button>" +
+	"</div>" +
+	"<div style='display: flex; width: 100%; position: absolute; justify-content: space-evenly; top: 180px;'>" +
+	"<div style='width: 40%;'>" +
+	"<p>Make the city yours. It will give you food and some troops over time.</p>" +
+	"</div>" +
+	"<div style='width: 40%;'>" +
+	"<p>Recruit all eligible people into your army, steal all the food, and burn the city down.</p>" +
+	"</div>" +
+	"</div>" +
+	"</div>" +
+	"</div>");
 
 /* Canvas */
 document.write("<canvas id='canvas' width='1347' height='587' style='border:2px solid black'></canvas>");
@@ -55,8 +55,12 @@ var timeSinceLastFrame = new Date();
 var mouseX = 0;
 var mouseY = 0;
 
+/* Letters */
+var consonants = ['q', 'w', 'r', 't', 'y', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+var vowels =['a', 'e', 'i', 'o', 'u'] ;
+
 /* Resize canvas to client's size */
-canvas.width  = window.innerWidth - 25;
+canvas.width	= window.innerWidth - 25;
 canvas.height = window.innerHeight - 25;
 
 /* City selected */
@@ -74,37 +78,37 @@ var keysPressed = [];
 
 /* Player's army */
 var army = {
-  x : 5000,
-  y : 5000,
-  troops : 500,
-  food: 1000
-  /* Size of the army is based on the amount of troops */
+	x : 5000,
+	y : 5000,
+	troops : 500,
+	food: 1000
+	/* Size of the army is based on the amount of troops */
 };
 
 /* Cities and Armies on the Map */
 var map = {
-  cities: [
+	cities: [
 
-  ],
-  armies: [
+	],
+	armies: [
 
-  ]
+	]
 };
 
 window.addEventListener("keydown", function(event) {
-  /* Get's keys pressed */
+	/* Get's keys pressed */
 
-  keysPressed[event.keyCode] = true;
+	keysPressed[event.keyCode] = true;
 
-  return true;
+	return true;
 });
 
 window.addEventListener("keyup", function(event) {
-  /* Get's keys unpressed */
+	/* Get's keys unpressed */
 
-  keysPressed[event.keyCode] = false;
+	keysPressed[event.keyCode] = false;
 
-  return true;
+	return true;
 });
 
 /* Get's mouse position */
@@ -115,38 +119,39 @@ canvas.addEventListener("mousemove", function(e) {
 
 /* Creates the cities and armies */
 function generateWorld(minX, maxX, minY, maxY, numCities, numArmies, minDistance) {
-  var people = 0;
+	var people = 0;
 
-  /* Generates Cities */
-  for (var i = 0; i < numCities; i ++) {
-    people = Math.floor((Math.random() * (army.troops/3)) + army.troops/3);
-    map.cities.push({
-      food: Math.floor((Math.random() * (people)) + 2 * people),
-      people: people,
-      x: (Math.random() * (maxX - minX)) + minX,
-      y: (Math.random() * (maxY - minY)) + minY,
-      health: people/25 /* people/25 is max health */
-    });
-  }
+	/* Generates Cities */
+	for (var i = 0; i < numCities; i ++) {
+		people = Math.floor((Math.random() * (army.troops/3)) + army.troops/3);
+		map.cities.push({
+			food: Math.floor((Math.random() * (people)) + 2 * people),
+			people: people,
+			x: (Math.random() * (maxX - minX)) + minX,
+			y: (Math.random() * (maxY - minY)) + minY,
+			health: people/25 /* people/25 is max health */,
+			name: "" + consonants[Math.floor(Math.random() * (consonants.length - 1))].toUpperCase() + vowels[Math.floor(Math.random() * (vowels.length - 1))] + consonants[Math.floor(Math.random() * (consonants.length - 1))] + vowels[Math.floor(Math.random() * (vowels.length - 1))] + consonants[Math.floor(Math.random() * (consonants.length - 1))] /* consonant, vowel, consonant, vowel, consonant */
+		});
+	}
 
-  /* Deletes cities that are too close to each other */
-  for (var i = 0; i < map.cities.length; i ++) {
-    for (var j = i + 1; j < map.cities.length; j ++) {
-      if (Math.sqrt(Math.pow(map.cities[i].x - map.cities[j].x, 2) + Math.pow(map.cities[i].y - map.cities[j].y, 2), 2) < minDistance) {
-        map.cities.splice(i, 1);
-        if (i > 0) {
-          i -= 1;
-        }
-        if (j > 0) {
-          j -= 1;
-        }
-      }
-    }
-  }
+	/* Deletes cities that are too close to each other */
+	for (var i = 0; i < map.cities.length; i ++) {
+		for (var j = i + 1; j < map.cities.length; j ++) {
+			if (Math.sqrt(Math.pow(map.cities[i].x - map.cities[j].x, 2) + Math.pow(map.cities[i].y - map.cities[j].y, 2), 2) < minDistance) {
+				map.cities.splice(i, 1);
+				if (i > 0) {
+					i -= 1;
+				}
+				if (j > 0) {
+					j -= 1;
+				}
+			}
+		}
+	}
 
-  /*for (var i = 0; i < numArmies; i ++) {
+	/*for (var i = 0; i < numArmies; i ++) {
 
-  }*/
+	}*/
 }
 
 /* Moves your army */
@@ -208,54 +213,48 @@ function drawInfoBox() {
 
 /* draws player army */
 function drawArmy() {
-  /* Draws Circle */
-	if (!blockading) {
-  	ctx.beginPath();
-  	ctx.arc(canvas.width/2, canvas.height/2, 40, 0, 2 * Math.PI);
-  	ctx.stroke();
-	} else if (blockading) {
-		ctx.beginPath();
-  	ctx.arc(canvas.width/2, canvas.height/2, 40 * (map.cities[citySelected].people/army.troops) + 10, 0, 2 * Math.PI);
-  	ctx.stroke();
-	}
+	/* Draws Circle */
+	ctx.beginPath();
+	ctx.arc(canvas.width/2, canvas.height/2, 40, 0, 2 * Math.PI);
+	ctx.stroke();
 }
 
 /* Draws all cities */
 function drawCities() {
-  for (var i = 0; i < map.cities.length; i ++) {
+	for (var i = 0; i < map.cities.length; i ++) {
 		/* Check if city is on screen */
-    if ((map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2 > - 100 &&
-      (map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2 < canvas.width + 100 &&
-      (map.cities[i].y - army.y)/army.troops * 1000 + canvas.height/2 > - 100 &&
-      (map.cities[i].y - army.y)/army.troops * 1000 + canvas.height/2 < canvas.height + 100) {
+		if ((map.cities[i].x - army.x)/500 * 1000 + canvas.width/2 > - 100 &&
+			(map.cities[i].x - army.x)/500 * 1000 + canvas.width/2 < canvas.width + 100 &&
+			(map.cities[i].y - army.y)/500 * 1000 + canvas.height/2 > - 100 &&
+			(map.cities[i].y - army.y)/500 * 1000 + canvas.height/2 < canvas.height + 100) {
 
-    	/* Draws Circles at Cities */
-      ctx.beginPath();
-	    ctx.arc((map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2, (map.cities[i].y - army.y)/army.troops * 1000 + canvas.height/2, 40 * (map.cities[i].people/army.troops), 0, 2 * Math.PI);
-	    ctx.fillStyle = "green";
-	    ctx.fill();
+			/* Draws Circles at Cities */
+			ctx.beginPath();
+			ctx.arc((map.cities[i].x - army.x)/500 * 1000 + canvas.width/2, (map.cities[i].y - army.y)/500 * 1000 + canvas.height/2, 40 * (25/40), 0, 2 * Math.PI);
+			ctx.fillStyle = "green";
+			ctx.fill();
 
-      /*  Text to label cities */
-      ctx.fillStyle = "black";
-      ctx.textAlign = "center";
-      ctx.font = 30 * (map.cities[i].people/army.troops) + "px Arial";
-      ctx.fillText("City " + (i + 1), (map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2, (map.cities[i].y - army.y)/army.troops * 1000 + canvas.height/2 - 45 * (map.cities[i].people/army.troops) - 20 * (map.cities[i].people/army.troops));
+			/*	Text to label cities */
+			ctx.fillStyle = "black";
+			ctx.textAlign = "center";
+			ctx.font = 30 * (25/40) + "px Arial";
+			ctx.fillText(map.cities[i].name, (map.cities[i].x - army.x)/500 * 1000 + canvas.width/2, (map.cities[i].y - army.y)/500 * 1000 + canvas.height/2 - 45 * (25/40) - 20 * (25/40));
 
-      /* Population */
-      ctx.font = 20 * (map.cities[i].people/army.troops) + "px Arial";
-      ctx.fillText(Math.floor(map.cities[i].people) + " people", (map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2, (map.cities[i].y - army.y)/army.troops * 1000 + canvas.height/2 + 57 * (map.cities[i].people/army.troops));
+			/* Population */
+			ctx.font = 20 * (25/40) + "px Arial";
+			ctx.fillText(Math.floor(map.cities[i].people) + " people", (map.cities[i].x - army.x)/500 * 1000 + canvas.width/2, (map.cities[i].y - army.y)/500 * 1000 + canvas.height/2 + 57 * (25/40));
 
-      /* Food */
-      ctx.font = 20 * (map.cities[i].people/army.troops) + "px Arial";
-      ctx.fillText(Math.floor(map.cities[i].food) + " food", (map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2, (map.cities[i].y - army.y)/army.troops * 1000 + canvas.height/2 + 77 * (map.cities[i].people/army.troops));
+			/* Food */
+			ctx.font = 20 * (25/40) + "px Arial";
+			ctx.fillText(Math.floor(map.cities[i].food) + " food", (map.cities[i].x - army.x)/500 * 1000 + canvas.width/2, (map.cities[i].y - army.y)/500 * 1000 + canvas.height/2 + 77 * (25/40));
 
 			/* Health Bar */
 			ctx.fillStyle = "red";
-			ctx.fillRect((map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2 - 50 * (map.cities[i].people/army.troops), (map.cities[i].y - army.y)/army.troops * 1000 + canvas.height/2 - 40 * (map.cities[i].people/army.troops) - 18 * (map.cities[i].people/army.troops), 100 * (map.cities[i].people/army.troops), 15 * (map.cities[i].people/army.troops));
+			ctx.fillRect((map.cities[i].x - army.x)/500 * 1000 + canvas.width/2 - 50 * (25/40), (map.cities[i].y - army.y)/500 * 1000 + canvas.height/2 - 40 * (25/40) - 18 * (25/40), 100 * (25/40), 15 * (25/40));
 			ctx.fillStyle = "green";
-			ctx.fillRect((map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2 - 50 * (map.cities[i].people/army.troops), (map.cities[i].y - army.y)/army.troops * 1000 + canvas.height/2 - 40 * (map.cities[i].people/army.troops) - 18 * (map.cities[i].people/army.troops), 100 * (map.cities[i].people/army.troops) * map.cities[i].health / (map.cities[i].people/25), 15 * (map.cities[i].people/army.troops));
+			ctx.fillRect((map.cities[i].x - army.x)/500 * 1000 + canvas.width/2 - 50 * (25/40), (map.cities[i].y - army.y)/500 * 1000 + canvas.height/2 - 40 * (25/40) - 18 * (25/40), 100 * (25/40) * map.cities[i].health / (map.cities[i].people/25), 15 * (25/40));
 		}
-  }
+	}
 }
 
 /* Updates Cities */
@@ -337,8 +336,8 @@ function attackMenu() {
 	/* City you're attacking */
 	ctx.fillStyle = "black";
 	ctx.textAlign = "left";
-  ctx.font = "20px Arial";
-	ctx.fillText("Attacking city " + (citySelected + 1), 10, 25);
+	ctx.font = "20px Arial";
+	ctx.fillText("Attacking: " + map.cities[citySelected].name, 10, 25);
 
 	/* Button for blockading a city */
 	document.getElementById('blockade-button').setAttribute("style", "outline:none; border-radius: 10px; display: block; position: absolute; height: 115px; width: 165px; left: 19px; top: 42px;");
@@ -355,12 +354,12 @@ document.onmouseup = function() {
 	/* Check if city is clicked */
 	for (var i = 0; i < map.cities.length; i ++) {
 		/* Check if city is rendered */
-		if ((map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2 > - 100 &&
-      (map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2 < canvas.width + 100 &&
-      (map.cities[i].y - army.y)/army.troops * 1000 + canvas.height/2 > - 100 &&
-      (map.cities[i].y - army.y)/army.troops * 1000 + canvas.height/2 < canvas.height + 100) {
+		if ((map.cities[i].x - army.x)/500 * 1000 + canvas.width/2 > - 100 &&
+			(map.cities[i].x - army.x)/500 * 1000 + canvas.width/2 < canvas.width + 100 &&
+			(map.cities[i].y - army.y)/500 * 1000 + canvas.height/2 > - 100 &&
+			(map.cities[i].y - army.y)/500 * 1000 + canvas.height/2 < canvas.height + 100) {
 
-			if (Math.sqrt(Math.pow((map.cities[i].x - army.x)/army.troops * 1000, 2) + Math.pow((map.cities[i].y - army.y)/army.troops * 1000, 2)) < 40 + 40 * (map.cities[i].people/army.troops) && Math.sqrt(Math.pow((map.cities[i].x - army.x)/army.troops * 1000 + canvas.width/2 - mouseX, 2) + Math.pow((map.cities[i].y - army.y)/army.troops * 1000 - mouseY + canvas.height/2, 2)) < 40 * (map.cities[i].people/army.troops)) {
+			if (Math.sqrt(Math.pow((map.cities[i].x - army.x)/500 * 1000, 2) + Math.pow((map.cities[i].y - army.y)/500 * 1000, 2)) < 40 + 40 * (25/40) && Math.sqrt(Math.pow((map.cities[i].x - army.x)/500 * 1000 + canvas.width/2 - mouseX, 2) + Math.pow((map.cities[i].y - army.y)/500 * 1000 - mouseY + canvas.height/2, 2)) < 40 * (25/40)) {
 					citySelected = i;
 			}
 
@@ -379,12 +378,12 @@ function draw() {
 	document.getElementById('attack-button').setAttribute("style", "outline:none; border-radius: 10px; display: none; position: absolute; height: 115px; width: 165px; left: 190px; top: 42px;");
 
 	/* Resize canvas to client's size */
-	canvas.width  = window.innerWidth - 25;
+	canvas.width = window.innerWidth - 25;
 	canvas.height = window.innerHeight - 25;
 
 	if (!pause) {
-  	/* Clears Canvas */
-  	ctx.clearRect(0, 0, canvas.width, canvas.height);
+		/* Clears Canvas */
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 		/* Draws Cities */
 		drawCities();
@@ -404,7 +403,7 @@ function draw() {
 		}
 	} else if (pause) {
 		/* Clears Canvas */
-  	ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 		/* Draws Cities */
 		drawCities();
@@ -429,7 +428,7 @@ function draw() {
 
 	timeSinceLastFrame = new Date();
 
-  requestAnimationFrame(draw);
+	requestAnimationFrame(draw);
 }
 
 draw();
